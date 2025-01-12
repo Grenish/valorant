6734fe7b18970830144f65902564808cbfe72c45
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-01-13 01:28:28 +0530

    frontend init

---
+++ client/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h2 className="valo-font text-5xl text-red-600 pointer-events-none select-none">Valorant</h2>
+    </div>
+  );
+}
